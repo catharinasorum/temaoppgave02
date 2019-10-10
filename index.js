65efe58fc35251215ac208c3d1cdcf29059d8bcb
Catharina Benedicte Sørum <catharina@Catharinas-MacBook-Pro.local>
2019-10-10 13:29:00 +0200

    Første endring

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 const sectionHandleliste = document.querySelector("#sectionHandleliste");
 const skjemaHandleliste = document.querySelector("#skjemaHandleliste");
 const inpVare = document.querySelector("#inpVare");
+const valgKategori = document.querySelector("#valgKategori");
 
-const varer = ["Melk", "Ost", "Egg", "Smør", "Jordbær", "Laks"];
+const varer = [
+    {navn: "Melk", kategori: "meieri"},
+    {navn: "Rømme", kategori: "frukt"},
+    {navn: "Fløte", kategori: "meieri"}
+];
 
 function visHandleliste() {
+
+    const meierivarer = varer.filter( vare => vare.kategori === "meieri" );
+    const fruktvarer = varer.filter( vare => vare.kategori === "frukt" );
+
+
     sectionHandleliste.innerHTML = "";
-    for(const [indeks, vare] of varer.entries()) {        
-        sectionHandleliste.innerHTML += `
-            <div>${vare}</div>
-            <div onClick="slett(${indeks})" class="btn">X</div>
-        `;
+
+    if(meierivarer.length > 0) {
+        sectionHandleliste.innerHTML += "<h2>Meieri</h2>";
+        for(const [indeks, vare] of meierivarer.entries()) {        
+            sectionHandleliste.innerHTML += `
+                <div>${vare.navn}</div>
+                <div onClick="slett(${indeks})" class="btn">X</div>
+            `;
     }
+    }
+
+    if(fruktvarer.length > 0) {
+        sectionHandleliste.innerHTML += "<h2>Frukt & Grønt</h2>";
+        for(const [indeks, vare] of fruktvarer.entries()) {        
+            sectionHandleliste.innerHTML += `
+                <div>${vare.navn}</div>
+                <div onClick="slett(${indeks})" class="btn">X</div>
+            `;
+    }  
+    }
+
 }
 
 
 function leggeTilVare(evt) {
     evt.preventDefault();
-    varer.unshift(inpVare.value);
+    varer.unshift(
+       {
+            navn: inpVare.value,
+            kategori: valgKategori.value
+       } 
+    );
     visHandleliste();
     skjemaHandleliste.reset();
 }
